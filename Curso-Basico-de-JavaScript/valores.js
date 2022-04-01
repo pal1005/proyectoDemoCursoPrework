fc997015f1a18de01995daceb8c740deeb1629f7
Tucco <miguepalacios1@gmail.com>
2022-03-31 20:12:24 -0500

    Nuevo curso

---
+++ Curso-Basico-de-JavaScript/valores.js
@@ -0,0 +1,13 @@
+40
+
+"Miguel Palacios"
+
+true
+false
+
+null
+
+undefined
+
+[1,2,3]
+{nombre: "Diego"}
---
+++ Curso-Basico-de-JavaScript/variables.js
@@ -0,0 +1,14 @@
+var nombre = "Miguel";
+// Declarar
+var edad;
+//inicializar
+edad = 30;
+
+//Arreglo variable
+var elementos = ["Compu", "Celular"];
+// Objeto
+var persona = {
+    nombre: "Miguel",
+    edad: 30
+}
+
